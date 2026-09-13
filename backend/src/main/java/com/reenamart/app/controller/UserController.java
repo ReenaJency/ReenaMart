74745d558039250d74e52a55b69eddfaea320fed
ReenaJency <reenajencyv@gmail.com>
2026-09-13 12:29:55 +0530

    Update ReenaMart backend and project files

--- backend/src/main/java/com/reenamart/app/controller/UserController.java
+++ backend/src/main/java/com/reenamart/app/controller/UserController.java
@@ -1,7 +1,7 @@
-
 package com.reenamart.app.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,9 +21,13 @@ public class UserController {
         this.userService = userService;
     }
 
+    @GetMapping
+    public String test() {
+        return "User API is working!";
+    }
+
     @PostMapping("/login")
     public User login(@RequestBody User user) {
-
         return userService.login(
             user.getUsername(),
             user.getPassword()
